9d75f0b4949ce1751fe17767b5c5dfe769905892
Connor Harwood <harwood.connor@gmail.com>
2014-06-12 19:10:09 -0400

    fill out migrations -user -game -round -color -audio

--- db/migrate/20140612221901_create_colors.rb
+++ db/migrate/20140612221901_create_colors.rb
@@ -1,7 +1,7 @@
 class CreateColors < ActiveRecord::Migration
   def change
     create_table :colors do |t|
-
+      t.string  :hexcode, required: true
       t.timestamps
     end
   end
--- db/migrate/20140612221913_create_audios.rb
+++ db/migrate/20140612221913_create_audios.rb
@@ -1,7 +1,7 @@
 class CreateAudios < ActiveRecord::Migration
   def change
     create_table :audios do |t|
-
+      t.string  :file_loc, required: true
       t.timestamps
     end
   end
