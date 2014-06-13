734a941b500dead14c4aed66a79e1eae63a4cce8
Connor Harwood <harwood.connor@gmail.com>
2014-06-13 13:11:03 -0400

    modify gamebuilder logic -remove json parsing -bypass strong params for game building(fix later) -add game#play view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,5 +5,20 @@ class GamesController < ApplicationController
 
   def create
     # Post route for saving game data 
+
+    # puts "[LOG] parameters: #{params.inspect}"
+
+    if GameBuilder.create_game(game_params)
+      redirect_to root_path
+    else
+      "no bueno"
+    end
+  end
+
+
+  private
+  def game_params
+    params.permit!          #(:n, :rounds)#.permit( (1..25).to_a.map!{|num| num.to_sym} )
   end
 end
+
--- app/models/game_builder.rb
+++ app/models/game_builder.rb
@@ -1,18 +1,18 @@
 class GameBuilder
-  def create_game(game_json)
-    game_hash = JSON.parse(game_json)
-    game = build_game(game_hash[:n], game_json)
-    game.rounds << build_rounds(game_hash[:rounds])
+  def self.create_game(game_params)
+    puts "[LOG] permitted params: #{game_params.inspect}"
+    game = build_game(game_hash["n"], game_hash)
+    game.rounds << build_rounds(game_hash["rounds"])
     game.save
   end
 
   private
 
-  def build_game(n, game_json)
-    User.find(session[:user_id]).games.build(n: n, json_string: game_json)
+  def self.build_game(n, game_hash)
+    User.find(1).games.build(n: n, json_string: game_hash)
   end
 
-  def build_rounds(rounds_hash)
+  def self.build_rounds(rounds_hash)
     new_rounds = []
     rounds_hash.each do |round_num, round_data|
       round_data[:round_number] = round_num.to_i
@@ -21,7 +21,9 @@ class GameBuilder
     new_rounds
   end
 
-  def build_round(round_data)
-    Round.build(round_data)
+  def self.build_round(round_data)
+    Round.new(round_data)
   end
+
+
 end
