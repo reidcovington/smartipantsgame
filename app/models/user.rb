1e9f1a87156573688066dcee0e0deb9f9ffc710d
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-13 15:48:23 -0400

    added email validation to make tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   
   has_many :games
+
+  validates :email, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }, presence: true
 end
