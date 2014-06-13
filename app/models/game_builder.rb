65c092fe955607981a198cbc7e012d448d0b592c
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-13 10:31:34 -0400

    create GameBuilder model and create initial methods - #create_game - #build_game - #build_rounds - #build_round

---
+++ app/models/game_builder.rb
@@ -0,0 +1,15 @@
+class GameBuilder
+  def create_game(game_hash)
+  end
+
+  private
+
+  def build_game(n)
+  end
+
+  def build_rounds(rounds_hash)
+  end
+
+  def build_round(round_hash)
+  end
+end
