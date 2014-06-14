8b9a0d8091909c1c23bdef2dae7df9264da92f6e
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-14 15:23:41 -0400

    add route validity test to all contexts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 
 describe UsersController do
-  context '#create'
-  context '#login'
-  context '#show'
-  context '#logout'
-  context '#update'
-  context '#destroy'
+  context '#create' do
+    it 'is a valid route'
+
+  end
+
+  context '#login' do
+    it 'is a valid route'
+  end
+
+  context '#show' do
+    it 'is a valid route'
+  end
+
+  context '#logout' do
+    it 'is a valid route'
+  end
+
+  context '#update' do
+    it 'is a valid route'
+  end
+  
+  context '#destroy' do
+    it 'is a valid route'
+  end
 end
