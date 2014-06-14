60ef068a12db96282f533616c1b620ef74f91251
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-14 15:20:08 -0400

    initial user controller testing skeleton

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe UsersController do
+  context '#create'
+  context '#login'
+  context '#show'
+  context '#logout'
+  context '#update'
+  context '#destroy'
+end
