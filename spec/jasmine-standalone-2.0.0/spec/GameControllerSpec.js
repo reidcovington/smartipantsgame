3ade80e02c72d8ba6a0e84be0b7649c2b1b9529b
reidcovington <reid@shopcube.com>
2014-06-14 15:13:09 -0400

    RoundModel jasmine tests, 10

---
+++ spec/jasmine-standalone-2.0.0/spec/GameControllerSpec.js
@@ -0,0 +1,31 @@
+describe("GameController", function() {
+
+  it("should test that jasmine is running", function() {
+    var player = "player";
+    expect(typeof player).toBe("string");
+  });
+
+
+  // describe("#initialize", function() {
+  //   var round =  new RoundModel( {colors: ["Magenta"], sounds: ["chewbacca growl"]});
+  //     it("creates a new RoundModel instance with the specified color attribute", function(){
+  //       expect(round.color).toEqual("Magenta");
+  //     });
+
+
+  // });
+
+  // describe("attribues", function() {
+  //   it("acceptes color and sound attributes", function(){
+  //     var round =  new RoundModel( {colors: ["Magenta"], sounds: ["chewbacca growl"]})
+  //     expect(round.color).toEqual("Magenta");
+  //     expect(round.sound).toEqual("chewbacca growl");
+  //   });
+
+  // });
+
+});
+
+
+
+
