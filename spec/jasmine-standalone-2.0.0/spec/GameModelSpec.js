4d66aa6fb9ba7208433afb80272227dc1580ad4f
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-14 16:02:59 -0400

    add Game Model tests - all passing

--- spec/jasmine-standalone-2.0.0/spec/GameModelSpec.js
+++ spec/jasmine-standalone-2.0.0/spec/GameModelSpec.js
@@ -1,10 +1,70 @@
+
+
 describe("GameModel", function() {
 
-  var gameModel;
+  describe("Game Model initialization", function() {
 
-  it("should test that jasmine is running", function() {
-    var player = "player"
-    expect(typeof player).toBe("string")
+    it("should be initialized with an n-back number and roundAttributes as arguments", function() {
+
+      var gameModel = new GameModel(2, { color: ['blue', 'green', 'orange'] })
+
+      expect(gameModel.n).toBeDefined();
+      expect(gameModel.roundAttributes).toBeDefined();
+      expect(gameModel).toBeDefined();
+    })
+
+    it("should have a rounds property which is an array", function() {
+
+      var gameModel = new GameModel(2, { color: ['blue', 'green', 'orange'] })
+      gameModel.rounds.push('test')
+
+      expect(gameModel.rounds[gameModel.rounds.length - 1]).toEqual('test')
+    })
   })
 
+  describe("GameModel methods", function() {
+    describe("#makeRounds", function() {
+      it('should initialize with a make rounds method', function() {
+        var gameModel = new GameModel(2, { color: ['blue', 'green', 'orange'] })
+        expect(gameModel.makeRounds).toBeDefined()
+      })
+
+      it("should create round objects and push them into gameModel.rounds", function() {
+        var gameModel = new GameModel(2, { colors: ['blue', 'green', 'red'] })
+
+        gameModel.rounds = []
+        gameModel.makeRounds()
+        expect(gameModel.rounds.length).toEqual(22);
+      })
+
+    describe('#scoreGuess', function() {
+      it('should set attributeGuess to true if user indicates a match correctly', function() {
+        var gameModel = new GameModel(2, { colors: ['blue', 'blue', 'blue', 'blue'] })
+        gameModel.scoreGuess('color', 3)
+        expect(gameModel.rounds[3]['colorGuess']).toEqual(true)
+      })
+
+      it('should set attributeGuess to true if user indicates a match correctly', function() {
+        var gameModel = new GameModel(2, { colors: [] })
+        gameModel.rounds[0].color = 'blue'
+        gameModel.rounds[2].color = 'red'
+        gameModel.scoreGuess('color', 2)
+        expect(gameModel.rounds[2]['colorGuess']).toBeUndefined();
+      })
+    })
+
+    describe('#scoreNonGuess', function() {
+      it('should set colorGuess to true if no guess was provided and there was no match', function() {
+        var gameModel = new GameModel(2, { colors: [] })
+
+        gameModel.rounds[0].color = 'blue'
+        gameModel.rounds[2].color = 'red'
+
+        gameModel.scoreNonGuess('color', 2)
+
+        expect(gameModel.rounds[2].colorGuess).toEqual(true)
+      })
+    })
+    })
+  })
 })
