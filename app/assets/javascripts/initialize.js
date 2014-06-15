0d62e5eb8828283c4f0239e8bf424b05daa0ee49
Connor Harwood <harwood.connor@gmail.com>
2014-06-15 15:41:38 -0400

    fix merge conflicts

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,11 +1,11 @@
 var gameData;
 $(document).ready(function() {
-    if (window.location.href.indexOf('/games/play') > -1){
-        $.get('/games/game_data').done(function(response){
-            gameData = response;
-        });
-       new ApplicationController("#game-section")
-    }
+    // if (window.location.href.indexOf('/games/play') > -1){
+    $.get('/games/game_data').done(function(response){
+        gameData = response;
+    });
+    new ApplicationController("#game-section")
+    // }
 
 })
 
