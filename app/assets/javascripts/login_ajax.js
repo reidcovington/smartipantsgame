db7bc9202d807b1efa04bb87b6f0c7993fcfe2d2
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-15 18:39:44 -0400

    add sign up ajax call

--- app/assets/javascripts/login_ajax.js
+++ app/assets/javascripts/login_ajax.js
@@ -1,9 +1,15 @@
   $(document).ready(function(){
     $('#submit-login').submit(function(e){
         e.preventDefault();
-        var userInfo = {email: $(emailinputselector).val(), password: $(passwordinputselector).val()}
+        var userInfo = {email: $(emailinputselector).val(), password: $(passwordinputselector).val()};
 // SIYAN: Change the above to match your form and debug.
         $.post('/users/login', userInfo);
+    });
+
+    $('submit-signup').submit(function(e){
+        e.preventDefault();
+        var userInfo = {username: $(usernameinputselector).val(), email: $(emailinputselector).val(), password: $(passwordinputselector).val()};
+        $.post('/users/create', userInfo);
     });
 
     $('#logout').click(function(e){
@@ -11,5 +17,6 @@
 
         $.get('/users/logout');
     });
+
   });
 
