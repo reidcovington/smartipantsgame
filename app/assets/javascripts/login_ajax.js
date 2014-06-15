2f6e1df6d11ccdc57466f4edf626d5c26f24e786
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-15 19:14:47 -0400

    add logic to switch layout based on logged in and out

--- app/assets/javascripts/login_ajax.js
+++ app/assets/javascripts/login_ajax.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
     $('#submit-login').submit(function(e){
         e.preventDefault();
-        var userInfo = {email: $(emailinputselector).val(), password: $(passwordinputselector).val()};
-// SIYAN: Change the above to match your form and debug.
-        $.post('/users/login', userInfo);
+        $.post('/users/login', $( this ).serialize()));      
     });
 
-    $('submit-signup').submit(function(e){
+    $('#submit-signup').submit(function(e){
         e.preventDefault();
         var userInfo = {username: $(usernameinputselector).val(), email: $(emailinputselector).val(), password: $(passwordinputselector).val()};
         $.post('/users/create', userInfo);
@@ -14,8 +12,6 @@ $(document).ready(function(){
 
     $('#logout').click(function(e){
         e.preventDefault();
-
         $.get('/users/logout');
     });
-
   });
