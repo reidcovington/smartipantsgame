0ad41bc9d946162d578e8aadca3fb25209a38299
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-15 19:38:38 -0400

    fix layout add sign up button

--- app/assets/javascripts/login_ajax.js
+++ app/assets/javascripts/login_ajax.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $('#submit-login').submit(function(e){
         e.preventDefault();
-        $.post('/users/login', $( this ).serialize()));      
+        $.post('/users/login', $( this ).serialize());      
     });
 
-    $('#submit-signup').submit(function(e){
+    $('#show-signup').click(function(e){
         e.preventDefault();
         var userInfo = {username: $(usernameinputselector).val(), email: $(emailinputselector).val(), password: $(passwordinputselector).val()};
         $.post('/users/create', userInfo);
