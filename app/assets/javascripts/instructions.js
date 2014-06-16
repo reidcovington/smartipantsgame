9f096a61d4a4c11070d9144a0f642f2121ea72a5
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-16 16:23:27 -0400

    add example nback in instructions

--- app/assets/javascripts/instructions.js
+++ app/assets/javascripts/instructions.js
@@ -8,4 +8,15 @@ $(document).ready(function() {
 		e.preventDefault();
 		$('#instructions-pane').hide();
 	})
+
 });
+	
+
+var playExample = function(target) {
+	exampleTarget = $( '#instructions-body' )
+	exampleTarget.empty();
+ 
+	$( "<td id='ex-1' class='btn' style='height:10em; width: 10em; background-color: blue; margin-right: .5em'></td>" ).appendTo(exampleTarget)
+	$( "<td id='ex-2' class='btn' style='height:10em; width: 10em; background-color: red; margin-right: .5em'></td>" ).appendTo(exampleTarget)
+	$( "<td id='ex-3' class='btn' style='height:10em; width: 10em; background-color: blue;'></td>" ).appendTo(exampleTarget)
+}
