1a7eb7d60091ea1f42eb28e36ab14359c9f34bdf
Steven Inouye <steven.inouye91@gmail.com>
2014-06-17 19:57:58 -0400

    re added the old logic for score page if old logic for score page is removed while, the new logic isnt working, the profile page total graph breaks -re added the old information with the new logic so that conno can do both

--- app/assets/javascripts/score_page.js
+++ app/assets/javascripts/score_page.js
@@ -51,7 +51,73 @@ var ready;
 ready = function() {
 
     if ((window.location.href.indexOf('profile'))>-1){
+        var valid_game = {
+            n: 2,
+            rounds: {
+                "1": {
+                    color_id: 1,
+                    audio_id: 1,
+                    color_correct: true,
+                    audio_correct: false
+                },
+                "3": {
+                    color_id: 2,
+                    audio_id: 3,
+                    color_correct: true,
+                    audio_correct: false
+                },
+                "2": {
+                    color_id: 4,
+                    audio_id: 2,
+                    color_correct: false,
+                    audio_correct: true
+                },
+                "4": {
+                    color_id: 4,
+                    audio_id: 2,
+                    color_correct: true,
+                    audio_correct: true
+                },
+                "5": {
+                    color_id: 1,
+                    audio_id: 1,
+                    color_correct: false,
+                    audio_correct: false
+                },
+                 "6": {
+                    color_id: 1,
+                    audio_id: 1,
+                    color_correct: true,
+                    audio_correct: false
+                }
+            }
+        }
+        var rounds = valid_game.rounds
+        var array_values = new Array();
+        var color_correct = new Array();
+        var audio_correct = new Array();
+        for (var key in rounds) {
+            array_values.push(rounds[key]);
+        }
+        console.log(array_values);
 
+        for (i=0; i<array_values.length; i++){
+          if(array_values[i].color_correct === true){
+              color_correct.push(array_values[i])
+          if(array_values[i].audio_correct === true){
+              audio_correct.push(array_values[i])
+            }
+          }
+        }
+
+        var total_correct_length = color_correct.length + audio_correct.length
+        var total_incorrect_length = 12 - total_correct_length
+        var hit = Math.round(total_correct_length / 12 * 100);
+        var miss = 100 - hit
+        var color_hit = Math.round((color_correct.length/total_correct_length) * hit)
+        var audio_hit = Math.round((audio_correct.length/total_correct_length) * hit)
+        var color_miss = Math.round(((6-color_correct.length)/total_incorrect_length) * miss)
+        var audio_miss = Math.round(((6-audio_correct.length)/total_incorrect_length) * miss)
 
 
         $(function () {
@@ -63,8 +129,8 @@ ready = function() {
                             color: colors[5],
                             drilldown: {
                                 name: 'Hit',
-                                categories: ['Color', 'Audio', 'Position'],
-                                data: [color_hit, audio_hit, position_hit],
+                                categories: ['Color', 'Audio'],
+                                data: [color_hit, audio_hit],
                                 color: colors[5]
                             }
                         }, {
@@ -72,8 +138,8 @@ ready = function() {
                             color: colors[2],
                             drilldown: {
                                 name: 'Miss',
-                                categories: ['Color', 'Audio', 'Position'],
-                                data: [color_miss, audio_miss, position_miss],
+                                categories: ['Color', 'Audio'],
+                                data: [color_miss, audio_miss],
                                 color: colors[2]
                             }
                         }];
