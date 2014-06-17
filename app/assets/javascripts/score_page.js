4e240145be37ce31c3d6af030ca2d55ba286465a
Steven Inouye <steven.inouye91@gmail.com>
2014-06-17 19:55:18 -0400

    end game graph logic

--- app/assets/javascripts/score_page.js
+++ app/assets/javascripts/score_page.js
@@ -4,10 +4,43 @@
 
 // ResultsGraphMaker.prototype = {
 //     makeBadassGraphdude: function(){
-//         this.tweekWeirdBits();
-//     }
+        // var score_n = gameObject[:n]
+        // var game_mode = gameObject[:gameMode]
+        // var score_game_rounds = gameObject[:rounds]
+        // var color_correct = new Array();
+        // var sound_correct = new Array();
+        // var position_correct = new Array();
+        // if(game_mode === "Single"){
+        //     game_mode_integer = 1
+        // }
+        // if(game_mode === "Dual"){
+        //     game_mode_integer = 2
+        // }
+        // if(game_mode === "Triple"){
+        //     game_mode_integer = 1
+        // }
+        //     for(i=0; i<score_game_rounds.length; i++){
+        //         if(score_game_rounds[(i+score_n)].colorGuess == true){
+        //             color_correct.push(score_game_rounds[(i+score_n)])
+        //         }
+        //         if(score_game_rounds[(i+score_n)].soundGuess == true){
+        //             sound_correct.push(score_game_rounds[(i+score_n)])
+        //         }
+        //         if(score_game_rounds[(i+score_n)].positionGuess == true){
+        //             position_correct.push(score_game_rounds[(i+score_n)])
+        //         }
+        //     }
+        //     var total_correct_length = color_correct.length + sound_correct.length + position_correct.length
+        //     var total_incorrect_length = (20*game_mode_integer) - total_correct_length
+        //     var hit = Math.round(total_correct_length / (20*game_mode_integer) * 100);
+        //     var miss = 100 - hit
+        //     var color_hit = Math.round((color_correct.length/total_correct_length) * hit)
+        //     var audio_hit = Math.round((sound_correct.length/total_correct_length) * hit)
+        //     var position_hit = Math.round((position_correct.length/total_correct_length) * hit)
+        //     var color_miss = Math.round(((20-color_correct.length)/total_incorrect_length) * miss)
+        //     var audio_miss = Math.round(((20-sound_correct.length)/total_incorrect_length) * miss)
+        //     var position_miss = Math.round(((20-position_correct.length)/total_incorrect_length) * miss)
 
-//     tweekWeirdBits: function(){
 
 //     }
 // }
@@ -18,72 +51,7 @@ var ready;
 ready = function() {
 
     if ((window.location.href.indexOf('profile'))>-1){
-        var valid_game = {
-            n: 2,
-            rounds: {
-                "1": {
-                    color_id: 1,
-                    audio_id: 1,
-                    color_correct: true,
-                    audio_correct: false
-                },
-                "3": {
-                    color_id: 2,
-                    audio_id: 3,
-                    color_correct: true,
-                    audio_correct: false
-                },
-                "2": {
-                    color_id: 4,
-                    audio_id: 2,
-                    color_correct: false,
-                    audio_correct: true
-                },
-                "4": {
-                    color_id: 4,
-                    audio_id: 2,
-                    color_correct: true,
-                    audio_correct: true
-                },
-                "5": {
-                    color_id: 1,
-                    audio_id: 1,
-                    color_correct: false,
-                    audio_correct: false
-                },
-                 "6": {
-                    color_id: 1,
-                    audio_id: 1,
-                    color_correct: true,
-                    audio_correct: false
-                }
-            }
-        }
-        var rounds = valid_game.rounds
-        var array_values = new Array();
-        var color_correct = new Array();
-        var audio_correct = new Array();
-        for (var key in rounds) {
-            array_values.push(rounds[key]);
-        }
 
-        for (i=0; i<array_values.length; i++){
-          if(array_values[i].color_correct === true){
-              color_correct.push(array_values[i])
-          if(array_values[i].audio_correct === true){
-              audio_correct.push(array_values[i])
-            }
-          }
-        }
-
-        var total_correct_length = color_correct.length + audio_correct.length
-        var total_incorrect_length = 12 - total_correct_length
-        var hit = Math.round(total_correct_length / 12 * 100);
-        var miss = 100 - hit
-        var color_hit = Math.round((color_correct.length/total_correct_length) * hit)
-        var audio_hit = Math.round((audio_correct.length/total_correct_length) * hit)
-        var color_miss = Math.round(((6-color_correct.length)/total_incorrect_length) * miss)
-        var audio_miss = Math.round(((6-audio_correct.length)/total_incorrect_length) * miss)
 
 
         $(function () {
@@ -95,8 +63,8 @@ ready = function() {
                             color: colors[5],
                             drilldown: {
                                 name: 'Hit',
-                                categories: ['Color', 'Audio'],
-                                data: [color_hit, audio_hit],
+                                categories: ['Color', 'Audio', 'Position'],
+                                data: [color_hit, audio_hit, position_hit],
                                 color: colors[5]
                             }
                         }, {
@@ -104,8 +72,8 @@ ready = function() {
                             color: colors[2],
                             drilldown: {
                                 name: 'Miss',
-                                categories: ['Color', 'Audio'],
-                                data: [color_miss, audio_miss],
+                                categories: ['Color', 'Audio', 'Position'],
+                                data: [color_miss, audio_miss, position_miss],
                                 color: colors[2]
                             }
                         }];
