2f5e5cac941bd02edb9e7df0c5a70a9a450fe2a2
Connor Harwood <harwood.connor@gmail.com>
2014-06-17 18:08:12 -0400

    separate initialize.js into jobs/roles -each job has its own file

---
+++ app/assets/javascripts/application_controller.js
@@ -0,0 +1,18 @@
+function ApplicationController(jQSelector){
+    this.jQSelector = jQSelector;
+    this.announcer = new Announcer(jQSelector, this);
+    this.gameController = null;
+    this.fetchStatData()
+};
+ApplicationController.prototype = {
+    buildGame: function(n, gameMode){
+        this.gameController = new GameController(n, gameMode, this.jQSelector, this);
+    },
+    announceResult: function(points, gameMode){
+        this.announcer.postResult(points, gameMode);
+    },
+    fetchStatData: function(){
+        $.get('/games/game_data').done(function(response){ gameData = response; });
+        $.get('/users/data').done(function(response){ stats = response; });
+    }
+};
