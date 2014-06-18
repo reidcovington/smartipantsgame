da38467bd9f40d84c106ca13bfe92990c17ec831
Connor Harwood <harwood.connor@gmail.com>
2014-06-18 15:38:02 -0400

    minor refactor to application_controller and announcer

--- app/assets/javascripts/application_controller.js
+++ app/assets/javascripts/application_controller.js
@@ -10,7 +10,15 @@ ApplicationController.prototype = {
         this.gameController = new GameController(n, gameMode, this.jQSelector, this);
     },
     announceResult: function(points, gameMode){
-        this.announcer.postResult(points, gameMode);
+        var rounds;
+        if (gameMode == 'single') {
+            rounds = 20;
+        } else if (gameMode == 'dual') {
+            rounds = 40;
+        } else if (gameMode == 'triple'){
+            rounds = 60;
+        };
+        this.announcer.postResult(points, rounds);
     },
     fetchStatData: function(){
         $.get('/games/game_data').done(function(response){ gameData = response; });
