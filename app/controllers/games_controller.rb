6a44b4fec5e508fcdd3d0bc143ec687a300e97a5
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-18 17:11:54 -0400

    update games_controller to pass tests

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,8 +8,8 @@ class GamesController < ApplicationController
   end
 
   def create
-    game_builder = GameBuilder.new(session[:user_id], game_params)
-    game_builder.create_game
+    @game_builder = GameBuilder.new(session[:user_id], game_params)
+    @game_builder.create_game
     redirect_to root_path
   end
 
