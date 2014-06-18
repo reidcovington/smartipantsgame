607e8585a6d512999781ef5392c3c8c42ae21e42
Steven Inouye <steven.inouye91@gmail.com>
2014-06-18 14:29:40 -0400

    refreactored profile page

---
+++ app/models/audio_graph_brain.rb
@@ -0,0 +1,19 @@
+class AudioGraphBrain
+  def self.user(player)
+    @user = player
+  end
+
+  def self.audio_true(player)
+    @audio_true_array = []
+    @user = User.find(player)
+    @game = @user.games.last
+    @true_rounds = @game.rounds.where(audio_correct: true)
+    @true_rounds.each do |round|
+        @audio_true_array << round.audio_id
+      end
+     @audio_hash = Hash.new(0)
+     @audio_true_array.each { | v | @audio_hash.store(v, @audio_hash[v]+1) }
+     @audio_hash
+  end
+
+end
---
+++ app/models/position_graph_brain.rb
@@ -0,0 +1,17 @@
+class PositionGraphBrain
+
+  def self.position_true(player)
+    @position_true_array = []
+    @user = User.find(player)
+    @game = @user.games.last
+    @true_rounds = @game.rounds.where(position_correct: true)
+    @true_rounds.each do |round|
+        @position_true_array << round.position
+      end
+      @position_true_array
+    @position_hash = Hash.new(0)
+    @position_true_array.each { | v | @position_hash.store(v, @position_hash[v]+1) }
+    @position_hash
+  end
+
+end
