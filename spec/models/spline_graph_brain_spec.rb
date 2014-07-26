1764fcf5a064afaf2604dadbc20b83497e649a3f
Stephen Roth <stephenroth.sr@gmail.com>
2014-07-26 19:31:11 -0400

    Add initial structure to spline graphs

--- spec/models/spline_graph_brain_spec.rb
+++ spec/models/spline_graph_brain_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe SplineGraphBrain do
+  context '#user' do
+  end
+
+  context '#game_dates' do
+  end
+
+  context '#color_correct' do
+  end
+
+  context '#audio_correct' do
+  end
+
+  context '#position_correct' do
+  end
+
+  context '#total_correct' do
+  end
+
+  context '#n' do
+  end
 end
